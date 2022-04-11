8ab8355ca1f052ba2c71fb7be450908a5f788ac4
NyaaRuss <nyasharusenazhou@gmail.com>
2022-04-11 20:13:29 +0200

    changed

--- Detection.py
+++ Detection.py
@@ -1,75 +1,146 @@
-
 import streamlit as st
 import cv2
-import tensorflow as tf 
+import tensorflow as tf
 import numpy as np
+import sys
 from keras.models import load_model
-from PIL import Image
-import PIL
 
-#Loading the Inception model
-model= load_model('mod.h5',compile=(False))
+# Loading the Inception model
+model = load_model('mod.h5', compile=(False))
 
-#Functions
-def splitting(name):
-    vidcap = cv2.VideoCapture(name)
-    success,frame = vidcap.read()
-    count = 0
-    frame_skip =1
-    while success:
-        success, frame = vidcap.read() # get next frame from video
-        cv2.imwrite(r"Images/frame%d.jpg" % count, frame) 
-        if count % frame_skip == 0: # only analyze every n=300 frames
-            print('frame: {}'.format(count)) 
-            pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) into PIL Image
-            st.image(pil_img)
-        if count > 20 :
-            break
-        count += 1
-    preprocessing()
 
-def preprocessing():
-    x = tf.io.read_file('Images/frame0.jpg')
-    x = tf.io.decode_image(x,channels=3) 
-    x = tf.image.resize(x,[299,299])
-    x = tf.expand_dims(x, axis=0)
-    x = tf.keras.applications.inception_v3.preprocess_input(x)
-    return x
-    
-def predict(x):
-    P = tf.keras.applications.inception_v3.decode_predictions(model.predict(x), top=1)
-    return P
-    
+# Functions
+def predict(frame, model):
+    # Pre-process the image for model prediction
+    img = cv2.resize(frame, (299, 299))
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+
+    img /= 255.0
+
+    # Predict with the Inceptionv3 model
+    prediction = model.predict(img)
+
+    # Convert the prediction into text
+    pred_text = tf.keras.applications.inception_v3.decode_predictions(
+        prediction, top=1)
+    for (i, (imagenetID, label, prob)) in enumerate(pred_text[0]):
+        label = ("{}: {:.2f}%".format(label, prob * 100))
+
+    st.markdown(label)
+
+
+def predict2(frame, model):
+    # Pre-process the image for model prediction
+    img = cv2.resize(frame, (299, 299))
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+
+    img /= 255.0
+
+    # Predict with the Inceptionv3 model
+    prediction = model.predict(img)
+
+    # Convert the prediction into text
+    pred_text = tf.keras.applications.inception_v3.decode_predictions(
+        prediction, top=1)
+    for (i, (imagenetID, label, prob)) in enumerate(pred_text[0]):
+        pred_class = label
+
+    return pred_class
+
+
+def object_detection(search_key, frame, model):
+    label = predict2(frame, model)
+    label = label.lower()
+    if label.find(search_key) > -1:
+        st.image(frame, caption=label)
+
+        # return sys.exit()
+    else:
+        pass
+        #st.text('Not Found')
+
+        # return sys.exit()
+
+
+# Main App
 def main():
+    """Deployment using Streamlit"""
     st.title("Object Detection")
+    st.text("Built with Streamlit and Inceptionv3")
 
-    file = st.file_uploader("Upload video",type=(['mp4']))
-    if file is not None: # run only when user uploads video
-        vid = file.name
-        with open(vid, mode='wb') as f:
-            f.write(file.read()) # save video 
+    activities = ["Detect Objects", "About"]
+    choice = st.sidebar.selectbox("Choose Activity", activities)
 
-        st.markdown(f"""
-        ### Files
-        - {vid}
-        """,
-        unsafe_allow_html=True) # display file name
+    if choice == "Detect Objects":
+        st.subheader("Upload Video")
 
-        video_file = open(vid, "rb").read()
+        video_file = st.file_uploader("Choose a video...", type=["mp4", "avi"])
 
-        st.video(video_file)
+        if video_file is not None:
+            path = video_file.name
+            with open(path, mode='wb') as f:
+                f.write(video_file.read())
+                st.success("Saved File")
+                video_file = open(path, "rb").read()
+                st.video(video_file)
+            cap = cv2.VideoCapture(path)
+            frame_width = int(cap.get(3))
+            frame_height = int(cap.get(4))
 
-        vidcap = cv2.VideoCapture(vid) # load video 
-        cur_frame = 0
-        success = True
-        
-    if st.button("Detect"):
-        output1 = splitting(vid)
-        output2 = preprocessing()
-        output = predict(output2)
-    
-        st.success(output)
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            output = cv2.VideoWriter(
+                'output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
-        
-if __name__=='__main__':
+            if st.button("Detect Objects"):
+
+                # Start the video prediction loop
+                while cap.isOpened():
+                    ret, frame = cap.read()
+
+                    if not ret:
+                        break
+
+                    # Perform object detection
+                    #frame = object_detection(frame, model)
+                    predict(frame, model)
+
+                    # Display the resulting frame
+                    #st.image(frame, caption='Video Stream', use_column_width=True)
+
+                cap.release()
+                output.release()
+                cv2.destroyAllWindows()
+
+            key = st.text_input('Search key')
+            key = key.lower()
+
+            if key is not None:
+
+                if st.button("Search for an object"):
+
+                    # Start the video prediction loop
+                    while cap.isOpened():
+                        ret, frame = cap.read()
+
+                        if not ret:
+                            break
+
+                        # Perform object detection
+                        object_detection(key, frame, model)
+                        #frame = predict(frame, model)
+
+                        # Display the resulting frame
+                        #st.image(frame, caption='Video Stream', use_column_width=True)
+
+                    cap.release()
+                    output.release()
+                    cv2.destroyAllWindows()
+
+    elif choice == "About":
+        st.subheader('Nyasha And Gabriella partnership')
+
+
+if __name__ == '__main__':
     main()
